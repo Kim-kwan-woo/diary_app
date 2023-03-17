c8f72e38306d9e550689bdbb55d29aa064ed0444
Kim-kwan-woo <kw2577@naver.com>
2023-03-17 14:23:41 +0900

    make util file

---
+++ lib/util/custom_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color primaryOlive = Color(0xff787D33);
+  static const Color primaryLime = Color(0xffE8F48C);
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color grey = Color(0xffD9D9D9);
+}
---
+++ lib/util/custom_text_style.dart
@@ -0,0 +1,16 @@
+import 'package:diary_app/util/custom_color.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextStyle {
+  static const h1BoldBlack = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    color: CustomColor.black,
+  );
+
+  static const h1BoldOlive = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    color: CustomColor.primaryOlive,
+  );
+}
