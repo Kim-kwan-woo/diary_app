61a3a3148396f2a1744e858721462fb296988b23
Kim-kwan-woo <kw2577@naver.com>
2023-03-19 01:33:19 +0900

    make login page

--- lib/view/page/login_page.dart
+++ lib/view/page/login_page.dart
@@ -1,3 +1,5 @@
+import 'package:diary_app/controller/login_controller.dart';
+import 'package:diary_app/util/app_routes.dart';
 import 'package:diary_app/util/custom_color.dart';
 import 'package:diary_app/util/custom_text_style.dart';
 import 'package:diary_app/view/widget/app_logo.dart';
@@ -5,41 +7,57 @@ import 'package:diary_app/view/widget/custom_elevated_button.dart';
 import 'package:diary_app/view/widget/custom_text_button.dart';
 import 'package:diary_app/view/widget/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends GetView<LoginController> {
   const LoginPage({super.key});
+  static const route = '/login';
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       resizeToAvoidBottomInset: false,
       body: SafeArea(
-        minimum: EdgeInsets.all(16),
+        minimum: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 50),
-            Text(
+            const SizedBox(height: 50),
+            const Text(
               style: CustomTextStyle.h2BoldBlack,
               '매일 쓰는\n나만의 다이어리',
             ),
-            SizedBox(height: 10),
-            AppLogo(),
-            SizedBox(height: 40),
+            const SizedBox(height: 10),
+            const AppLogo(),
+            const SizedBox(height: 40),
             //이메일 입력 필드
-            CustomTextField(hintText: '이메일을 입력하세요.'),
-            SizedBox(height: 20),
-            //비밀번호 입력 필드
-            CustomTextField(hintText: '비밀번호를 입력하세요.'),
-            SizedBox(height: 46),
-            //이메일 로그인 버튼
-            CustomElevatedButton(
-              backgroundColor: CustomColor.primaryOlive,
-              childText: '로그인',
+            CustomTextField(
+              controller: controller.emailController,
+              hintText: '이메일을 입력하세요.',
+              onChanged: (_) => controller.activateLoginButton(),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 20),
+            //비밀번호 입력 필드
+            CustomTextField(
+              controller: controller.pwController,
+              obscureText: true,
+              hintText: '비밀번호를 입력하세요.',
+              onChanged: (_) => controller.activateLoginButton(),
+            ),
+            const SizedBox(height: 46),
+            //이메일 로그인 버튼
+            Obx(
+              () => CustomElevatedButton(
+                onPressed: controller.login,
+                isActivated: controller.isButtonActivated.value,
+                backgroundColor: CustomColor.primaryOlive,
+                childText: '로그인',
+              ),
+            ),
+            const SizedBox(height: 10),
             //구글 로그인 버튼
             CustomElevatedButton(
+              onPressed: controller.signInWithGoogle,
               backgroundColor: CustomColor.primaryLime,
               childText: '구글로 로그인하기',
             ),
@@ -48,6 +66,7 @@ class LoginPage extends StatelessWidget {
               alignment: Alignment.centerRight,
               child: CustomTextButton(
                 childText: '회원가입',
+                onPressed: () => Get.toNamed(AppRoutes.signup),
               ),
             ),
           ],
