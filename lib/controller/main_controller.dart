40975584e129076d2ba5b1aa08fc07b5a66c8cae
Kim-kwan-woo <kw2577@naver.com>
2023-03-19 22:18:09 +0900

    make main controller

---
+++ lib/controller/main_controller.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+import 'package:diary_app/controller/auth_controller.dart';
+import 'package:diary_app/service/storage_service.dart';
+import 'package:diary_app/view/widget/custom_bottom_sheet.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+
+class MainController extends GetxController {
+  RxList<bool> selections = [true, false].obs; //하단 토글 버튼 선택 옵션
+  var pageViewController = PageController(); //페이지뷰 컨트롤러
+  Rxn<String> downloadUrl = Rxn<String>(
+      Get.find<AuthController>().user.value!.photoURL); //프로필 이미지 Url
+
+  //유저 정보 가져오기
+  Rxn<User> getUser() => Get.find<AuthController>().user;
+
+  //페이지 이동
+  jumpToPage(int page) => pageViewController.jumpToPage(page);
+
+  /*내 정보 페이지*/
+  //로그아웃
+  logout() => Get.find<AuthController>().logout();
+
+  //프로필 사진 업로드
+  uploadProfileImage(BottomSheetType option) async {
+    var picker = ImagePicker();
+    XFile? res;
+    if (option == BottomSheetType.first) {
+      res = await picker.pickImage(source: ImageSource.camera);
+    } else {
+      res = await picker.pickImage(source: ImageSource.gallery);
+    }
+
+    if (res != null) {
+      //AuthController 가져오기
+      var controller = Get.find<AuthController>();
+      downloadUrl(await StorageService()
+          .uploadProfileImage(controller.user.value!, File(res.path)));
+
+      controller.uploadProfileImage(downloadUrl.value!);
+    }
+  }
+}
