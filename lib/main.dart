de683ff54e54c7f26384e8094bef8465b96cd93b
Kim-kwan-woo <kw2577@naver.com>
2023-03-20 22:15:50 +0900

    스플래시 화면 구현

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,13 @@ import 'package:diary_app/util/app_pages.dart';
 import 'package:diary_app/view/page/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 void main() async {
-  //firebase 초기화
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding); //스플래시 화면 설정
+  //파이어베이스 초기화
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -36,6 +38,7 @@ class DiaryApp extends StatelessWidget {
         Get.lazyPut(() => DiaryDetailController(), fenix: true);
         Get.lazyPut(() => NoteAddController(), fenix: true);
         Get.lazyPut(() => NoteDetailController(), fenix: true);
+        FlutterNativeSplash.remove(); //스플래시 화면 제거
       }),
       getPages: AppPages.pages,
       home: const LoginPage(),
